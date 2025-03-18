cfa26fba92efe660eecb112c06750c1d90143ae6
djcadaniel <djcadaniel@gmail.com>
2025-03-18 16:39:57 -0500

    deploy vercel

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ const routerApi = require('./routes');
 const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/errorHandler')
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
@@ -23,7 +23,7 @@ const options = {
 
 app.use(cors(options));
 
-app.get('/', (req, res)=>{
+app.get('/api', (req, res)=>{
   res.send('hola fullstack djcadaniel, este es pmi primer server')
 })
 
